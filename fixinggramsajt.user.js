99bb15ad78503599a2350215d7f62008850d8c88
Marcin <ziptofaf@tuta.io>
2015-11-04 17:56:08 +0100

    i jeszcze na stronie glownej

--- fixinggramsajt.user.js
+++ fixinggramsajt.user.js
@@ -4,10 +4,13 @@
 // @description Zbior skryptow które doprowadzaja gramsajty do uzyteczności
 // @downloadURL https://raw.githubusercontent.com/ziptofaf/fixinggram/master/fixinggramsajt.user.js
 // @include     http://ja.gram.pl/*
+// @grant    GM_addStyle
+// @grant    GM_getResourceText
 // @require http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @require http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js
 // @version     1
 // ==/UserScript==
 $(document).ready(function() {
-$(".ico").attr("width", "32px");
+  $(".av .ico").attr("width", "32px");
+  $(".av img").attr("width", "32px");
 });
