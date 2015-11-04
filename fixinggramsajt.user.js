c1f2210d357ab1643aa1459c261f637229e261af
Marcin <ziptofaf@tuta.io>
2015-11-04 17:32:31 +0100

    naprawiamy gsy

---
+++ fixinggramsajt.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        FixingGramSajt
+// @namespace   ziptofaf2
+// @description Zbior skryptow które doprowadzaja gramsajty do uzyteczności
+// @downloadURL https://raw.githubusercontent.com/ziptofaf/fixinggram/master/fixinggramsajt.user.js
+// @include     http://ja.gram.pl/*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @require http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js
+// @version     1
+// ==/UserScript==
+$(document).ready(function() {
+$(".ico").attr("width", "32px");
+});
